9c9285ad6f3a0a262a428854387b31101df6387d
JHFellows <j.fellows88@gmail.com>
2017-06-28 16:54:41 -0400

    if user is signed in App.js renders Tabs, if not App.js renders LoginForm

--- App.js
+++ App.js
@@ -8,23 +8,40 @@ import LoginForm from './components/LoginForm'
 import store from './store';
 import {auth} from './firebase'
 
-auth.onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    console.log("auth state changed, user is", user)
-    //generally confused about how this is working
-    // when you first sign in nothing happens but when you refresh the page 
-    // you get the console.log
-  } else {
-    // No user is signed in.
-    "oh no no user is signed in "
-  }
-});
-
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in.
+        console.log("auth state changed, user is", user)
+        //generally confused about how this is working
+        // when you first sign in nothing happens but when you refresh the page
+        // you get the console.log
+        this.setState({user});
+      } else {
+        // No user is signed in.
+        console.log("user logged out", this);
+        this.setState({user:null})
+      }
+    });
+  }
 
   render() {
+    if(this.state.user){
+      return (
+        <Provider store={store}>
+          <Tabs />
+        </Provider>
+      )
+    }
     return (
       <Provider store={store}>
         <LoginForm />
@@ -32,4 +49,3 @@ export default class App extends Component {
     );
   }
 }
-
