10691894e3216a82ee4d73f8de5a2e1d9306d87f
Allie Fauer <alliefauer@gmail.com>
2017-06-28 15:51:13 -0400

    Remove the now ignored directory node_modules

--- node_modules/react-navigation/lib-rn/PlatformHelpers.native.js
+++
@@ -1,5 +0,0 @@
-import { BackAndroid as DeprecatedBackAndroid, BackHandler, Linking } from 'react-native';
-
-const BackAndroid = BackHandler || DeprecatedBackAndroid;
-
-export { BackAndroid, Linking };
