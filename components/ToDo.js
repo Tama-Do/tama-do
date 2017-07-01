7d5dfa12b0b083b07affa8804dee2799e06414e1
Katie Surrence <katie@Katies-Air.home>
2017-07-01 15:10:20 -0400

    removed some console logs

--- components/ToDo.js
+++ components/ToDo.js
@@ -7,8 +7,6 @@ import store from '../store'
 import Checkbox from './common/checkbox'
 import database from '../firebase'
 
-
-
 class ToDo extends Component {
   constructor(props) {
     super(props)
@@ -46,11 +44,9 @@ class ToDo extends Component {
       .then(treatType => {
         //what if task does not yet have a treatType associated?
         if (!treatType) {
-          //treatType = this.getTreatType()
-          treatType = 'anger'
+          treatType = this.getTreatType()
           // update task to have treat type
           taskRef.update({ treat: treatType })
-          console.log("I should only be here if treat type is unassigned")
         }
         return treatType
       })
@@ -109,89 +105,21 @@ class ToDo extends Component {
     })
   }
 
-
   onChange(userId, taskId, completed) {
-    console.log("we're heerrre !")
     var taskUpdates = {}
     var treatsRef = database.ref(`/users/${this.state.auth.user}`).child('treats')
     var taskRef = database.ref(`/users/${this.state.auth.user}/tasks/${taskId}`)
 
-
     if (completed) {
-      console.log("we're unchecking a checked box")
       taskUpdates = { completed: false }
       this.subtractTreat(treatsRef, taskRef)
     } else {
-      console.log("we're checking an unchecked box")
       taskUpdates = { completed: true }
       this.addTreat(treatsRef, taskRef)
     }
     database.ref(`/users/${userId}/tasks/${taskId}`).update(taskUpdates)
   }
 
-
-  // if (completed) {
-  //   // if you "uncomplete a task"
-  //   taskUpdates = { completed: false }
-  //   // you need to get the treat associated with that task, and decrement it.
-  //   // first we need to read the treat type from the task
-
-
-
-  //   // treatsRef.orderByChild("task").equalTo(taskId).once("value", function (snapshot) {
-  //   //   snapshot.forEach(function (child) {
-  //   //     updates[child.key] = null;
-  //   //   })
-  //   // })
-  // } else {
-  //   taskUpdates = { completed: true }
-
-  //   //addTreat(treatsRef, taskRef)
-
-  //   // at the point at which you complete a task
-  //   // ask: does this task already have a treat assigned?
-
-  //   taskRef.once('value').then(function (snapshot) {
-  //     var treatType
-  //     var treat = snapshot.val().treat;
-  //      // if not, assign a treat
-  //     if (!treat) {
-  //       treatType = getTreatType()
-  //       taskUpdates['treat'] = treatType
-  //     } else {
-  //       //need to read the treatType from the task
-  //       treatType = snapshot.val().treat.type
-  //     }
-
-  //     // ok, now you know the task has a treat
-
-  //     // now you need to find the treat and increment it by one
-
-  //     treatsRef.orderByChild("type").equalTo(treatType).once('value').then(function (snapshot) {
-  //       // does that treat already exist on /treats? 
-  //       if (snapshot.length == 0) {
-  //         // if not, create it, with an initial quantity of 1
-  //         var newTreatRef = treatsRef.push()
-  //         newTreatRef.set({
-  //           type: treatType,
-  //           quantity: 1
-  //         })
-  //       } else {
-  //         // if so, find it
-  //         snapshot.forEach(function (child) {
-  //           //get current quantity
-  //           var quant = snapshot.val().quantity
-  //           //add one
-  //           quant = (Number(currQuant) + 1).toString()
-  //           //update 
-  //           child.update({ quantity: quant })
-  //         })
-  //       }
-  //     }),
-
-  //   database.ref(`/users/${userId}/tasks/${taskId}`).update(updates)
-  // },
-  // },
   _keyExtractor = (item) => item.key
 
   render() {
@@ -203,13 +131,11 @@ class ToDo extends Component {
           removeClippedSubviews={false}
           keyExtractor={this._keyExtractor}
           renderItem={({ item }) => {
-            console.log("completed ", item.completed)
             return (
               <View style={styles.listItem}>
                 <Checkbox
                   label={item.name}
                   onChange={() => {
-                    console.log("got here!")
                     this.onChange(this.state.auth.user, item.key, item.completed)}}
                   checked={item.completed}
                 />
