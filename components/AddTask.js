7d91cd2a09b94e0de85ff91f0cc1c569fd7c781d
Katie Surrence <katie@Katies-Air.home>
2017-07-04 09:35:40 -0400

    modal styling in progress

--- components/AddTask.js
+++ components/AddTask.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, Modal, TouchableHighlight, TouchableOpacity, TextInput, Button, Image, StyleSheet } from 'react-native'
-import { Card, CardSection, Input, Spinner } from './common';
+import {
+    View,
+    Text,
+    TextInput,
+    Modal,
+    TouchableHighlight,
+    TouchableOpacity,
+    Button,
+    Image,
+    StyleSheet
+} from 'react-native'
 import database from '../firebase'
 import store from '../store'
 
@@ -50,36 +59,28 @@ export default class AddTaskModal extends Component {
                     visible={this.state.modalVisible}
                     onRequestClose={() => { alert("Modal has been closed.") }}
                 >
-                    <View style={{ flex: 1, position: 'absolute', top: 150 }}>
-                        <View>
-                            <CardSection>
-                            <View style={{position: 'absolute', height: 25, right: 10}}>
-                             
-                            </View>
-                                <Input
-                                    label="NewTask"
-                                    placeholder="new task"
-                                    onChangeText={(task) => this.setState({ task })}
-                                    value={this.state.task}
-                                />
-                            </CardSection>
-                            <CardSection>
-                            <TouchableHighlight style={{height: 25, zindex: 200, paddingTop: 10, paddingBottom:10}} onPress={() => {
-                                    this.setModalVisible(!this.state.modalVisible)
-                                }}>
-                                    <Text>Go back to task list</Text>
+                    <View style={styles.modalContainer}>
+                        <View style={styles.bodyContainer}>
+                            <View style={styles.headerContainer}>
+                                <Text style={styles.headerTextStyle}> NEW TASK </Text>
+                                <TouchableHighlight
+                                    style={{ height: 25, paddingTop: 4, paddingBottom: 10 }}
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisible)
+                                    }}>
+                                    <Text style={{ fontSize: 16, color: 'white', fontWeight: 'bold' }}>   x   </Text>
                                 </TouchableHighlight>
-                            </CardSection>
-                            <CardSection>
-                                {/*<Button title={'Add Task'} onPress={()=>{this.onButtonPress()}}>Add Task</Button>*/}
-                                <TouchableOpacity onPress={() => { this.onButtonPress() }}>
-                                    <Image source={require('./common/paw_check_mark-03.png')} />
-
-                                </TouchableOpacity>
-                            </CardSection>
-                        
-                               
-                            
+                            </View>
+                            <View>
+                            <TextInput style={{height:25, textAlign: 'center', backgroundColor: 'white', justifyContent: 'center' }}
+                                placeholder="task description"
+                                onChangeText={(task) => this.setState({ task })}
+                                value={this.state.task}
+                            />
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                {<Button style={styles.buttonStyle} title={'ADD NEW TASK'} onPress={() => { this.onButtonPress() }} />}
+                            </View>
 
                         </View>
                     </View>
@@ -99,7 +100,7 @@ export default class AddTaskModal extends Component {
 const styles = StyleSheet.create({
     pawprintContainer: {
         flex: 1,
-        
+
     },
     pawprint: {
         position: 'absolute',
@@ -114,5 +115,61 @@ const styles = StyleSheet.create({
     x: {
         position: 'absolute',
         right: 10
+    },
+
+    buttonContainer: {
+        position: 'absolute',
+        left: 10,
+        right: 10,
+        bottom: 15,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#8061A9'
+    },
+    buttonStyle: {
+        flex: 1,
+        borderWidth: 1,
+        borderRadius: 20,
+        backgroundColor: '#8061A9',
+        borderColor: '#6D45AD',
+        color: 'white',
+        marginLeft: 10,
+        marginRight: 10
+
+    },
+    modalContainer: {
+        flex: 1,
+        alignSelf:
+        'stretch',
+        top: 160
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        paddingLeft: 10,
+        paddingRight: 10,
+        height: 50,
+        justifyContent: 'space-between',
+        backgroundColor: '#8061A9',
+        marginBottom: 20
+    },
+    bodyContainer: {
+        backgroundColor: 'white',
+        height: 175,
+        flexDirection: 'column',
+        justifyItems: 'center',
+        borderBottomRadius: .1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#D9D9D9',
+        borderTopRadius: .1,
+        borderTopWidth: 1,
+        borderTopColor: '#D9D9D9'
+    },
+    headerTextStyle: {
+        paddingTop: 6,
+        paddingBottom: 10,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white'
     }
+
 })
