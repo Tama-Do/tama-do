bd56f638927e14d23e1d385177da73ff390493a5
Stella Chung <stll.chung@gmail.com>
2017-07-06 10:44:23 -0400

    basic date logging set up

--- components/helpers/distance.js
+++ components/helpers/distance.js
@@ -3,32 +3,33 @@ import { Constants, Location, Permissions } from 'expo';
 // calculate distance in feet between the user and the pet
 // lat1 and lon1 are the user's location
 // lat2 and lon2 are the pet's location
-export function distance (lat2, lon2) {
-  Number.prototype.toRad = function() {
-   return this * Math.PI / 180;
+export function distance(lat2, lon2, userId, petKey) {
+  Number.prototype.toRad = function () {
+    return this * Math.PI / 180;
   }
   let lat1, lon1;
   _getLocationAsync()
-  .then(({latitude, longitude}) => {
-    lat1 = latitude;
-    lon1 = longitude;
-    var R = 6371e3; // metres
-    var φ1 = lat1.toRad();
-    var φ2 = lat2.toRad();
-    var Δφ = (lat2-lat1).toRad();
-    var Δλ = (lon2-lon1).toRad();
-    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = (R * c * 3.2808).toFixed();
-    var bool = d <= 200;
-    this.setState({checkedIn: bool});
-    if (!bool) {
-      this.setState({animationType: 'DISGUST'})
-    }
-  })
-  .catch(console.error);
+    .then(({ latitude, longitude }) => {
+      lat1 = latitude;
+      lon1 = longitude;
+      var R = 6371e3; // metres
+      var φ1 = lat1.toRad();
+      var φ2 = lat2.toRad();
+      var Δφ = (lat2 - lat1).toRad();
+      var Δλ = (lon2 - lon1).toRad();
+      var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = (R * c * 3.2808).toFixed();
+      var bool = d <= 200;
+      this.setState({ checkedIn: bool });
+      if (!bool) {
+        this.setState({ animationType: 'DISGUST' })
+      }
+      dateVisited.call(this, userId, petKey, bool)
+    })
+    .catch(console.error);
 };
 
 // async request to get the user's location
@@ -40,6 +41,33 @@ export const _getLocationAsync = async () => {
     //   errorMessage: 'Permission to access location was denied',
     // });
   }
-  let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+  let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
   return { latitude: location.coords.latitude, longitude: location.coords.longitude };
 };
+
+function dateVisited(userId, petKey, checkedIn) {
+  if (!this.state.pet.dates && !checkedIn) {
+    return null;
+  }
+
+  let lastDate = Object.keys(this.state.pet.dates).map(dateMS => Number(dateMS))
+  lastDate = this.state.pet.dates[Math.max(...lastDate)]
+  if (checkedIn) {
+    let today = new Date();
+    let date = today.getMonth() + '-' + today.getDate() + '-' + today.getFullYear()
+    let dateMS = Date.now()
+    if (!this.state.pet.dates) {
+      this.props.addPetDate(userId, petKey, dateMS, date)
+      this.setState({ lastVisit: date })
+    } else {
+      if (lastDate !== date) {
+        this.props.addPetDate(userId, petKey, dateMS, date)
+        this.setState({ lastVisit: date })
+      } else {
+        this.setState({ lastVisit: lastDate })
+      }
+    }
+  } else {
+    this.setState({ lastVisit: lastDate });
+  }
+}
